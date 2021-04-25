43823d74bc89d4589cb8ba0f3e87d0f0c1f5321c
김민섭 <33247303+meeansub@users.noreply.github.com>
2021-04-25 23:55:16 +0900

    etc: @controller

--- src/main/java/com/sublivan/movie/controller/MovieController.java
+++ src/main/java/com/sublivan/movie/controller/MovieController.java
@@ -5,13 +5,13 @@ import com.sublivan.movie.dto.Movie;
 import com.sublivan.movie.service.MovieService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class MovieController {
     private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 
--- src/main/java/com/sublivan/movie/controller/ScheduleController.java
+++ src/main/java/com/sublivan/movie/controller/ScheduleController.java
@@ -1,8 +1,8 @@
 package com.sublivan.movie.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class ScheduleController {
 
 //    @Autowired
