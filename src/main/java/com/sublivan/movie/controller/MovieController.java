014e89d608d74bf37680adb45317eb891f1fd263
김민섭 <33247303+meeansub@users.noreply.github.com>
2021-04-25 14:49:24 +0900

    other

--- src/main/java/com/sublivan/movie/controller/MovieController.java
+++ src/main/java/com/sublivan/movie/controller/MovieController.java
@@ -1,17 +1,22 @@
 package com.sublivan.movie.controller;
 
+import ch.qos.logback.classic.Logger;
 import com.sublivan.movie.dto.Movie;
 import com.sublivan.movie.service.MovieService;
 import com.sublivan.movie.service.ScheduleService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
 public class MovieController {
+    private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public MovieService movieService;
